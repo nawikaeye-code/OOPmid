fafc0fc2879d19718fc38eff937b58b4c6f9e897
Boonraksa <nawikaeye@gmail.com>
2026-01-06 15:21:57 +0700

    bankka

--- bank.py
+++ bank.py
@@ -2,3 +2,13 @@ class BankAccountgit:
     def __init__(self,balance=0):
         self.__balance = balance
     def deposit(self,amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"ฝากเงินจำนวน {amount} สำเร็จ")
+        else:
+            print("ยอดเงินไม่เพียงพอ")
+    
+    def withdraw(self,amount):
+        if amount > 0 and :
+            print("ยอดเงินไม่เพียงพอ")
+        elif
